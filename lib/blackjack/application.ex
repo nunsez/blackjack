ac995440e08805aee02802c1200bc07489159740
Alexander Mandrikov <mandrikov@pm.me>
2024-03-19 15:53:52 +0700

    init app

---
+++ lib/blackjack/application.ex
@@ -0,0 +1,20 @@
+defmodule Blackjack.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl Application
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Blackjack.Worker.start_link(arg)
+      # {Blackjack.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Blackjack.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
